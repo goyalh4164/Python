8ebe66802d12bffef359ae617440e7aa16a693f9
Harsh <goyalh4164@gmail.com>
2023-04-07 12:08:02 +0530

    Datatypes

---
+++ Learn Python/Datatypes.py
@@ -0,0 +1,17 @@
+# Fundamental Datatypes
+int 
+float 
+bool
+str
+list
+tuple
+set
+dict
+
+#Classes ->custom types
+
+#Specialized Data Types
+# We can use these by using Modules
+
+# None os also a datatype
+None 
