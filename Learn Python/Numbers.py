7aaa9e7f35c7ba9c1b725e8fc2ba4bc104537d52
Harsh <goyalh4164@gmail.com>
2023-04-07 13:53:44 +0530

    Numbers

---
+++ Learn Python/Numbers.py
@@ -0,0 +1,32 @@
+# int and float
+
+print(type(6))
+print(type(2-4))
+print(type(2*4))
+print(type(2/4))
+print(type(20+1.1))
+print(type(9.9+1.1))
+
+# floating number requires more space
+
+# Some interesting operations
+
+# power
+print(2**3)
+
+# round off division
+print(5//4)  
+
+# Modulo
+print(6%4)
+
+# Math Functions
+
+# Round
+print(round(3.4))
+print(round(3.5))
+print(round(3.6))
+
+#abs
+print(abs(7-10))
+print(abs(-20))
