917fbc0a77d496d145b20fc6ae09090ebd2527c2
Harsh <goyalh4164@gmail.com>
2023-04-09 17:21:01 +0530

    modules

---
+++ Modules/main.py
@@ -0,0 +1,10 @@
+# package is a collection of modules
+# modules is a collection of functions
+# with a package we need a init file
+import shoping.shoping
+import utility
+
+print(utility) #it will cache the file for faster processing
+print(utility.divide(2,3))
+print(utility.multiply(2,3))
+print(shoping.shoping.buy('apple'))
